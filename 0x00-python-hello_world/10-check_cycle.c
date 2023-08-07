232617715f248a4e58fa2a1be60922a83dcd3242
Albert Mpepo <albertmpepo@gmail.com>
2023-08-07 06:53:49 -0700

    cycle checker

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - detects a cycle in singly linked list.
+ * @list: the iterated list
+ *
+ * Return: 0 (No cycle), 1 (There is a cycle)
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+		return (0);
+
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
