9557878a257080785bfc08225263d83288017bde
Albert Mpepo <albertmpepo@gmail.com>
2023-08-08 05:39:30 -0700

    tasks completed expect 6 and C

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,7 @@
 def uppercase(str):
     for i in str:
         num = ord(i)
-
-        def print_word():
-            print("{}".format(chr(num)), end="")
         if num >= 97 and num <= 122:
             num -= 32
-            print_word()
-        else:
-            print_word()
+        print("{}".format(chr(num)), end=""
     print("\n", end="")
