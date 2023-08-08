c67074f00f162966aef70d85f5d81fb56adbfb01
Albert Mpepo <albertmpepo@gmail.com>
2023-08-08 03:06:09 -0700

    all mandatory except 6

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        num = ord(i)
+
+        def print_word():
+            print(chr(num), end="")
+        if num >= 97 and num <= 122:
+            num -= 32
+            print_word()
+        else:
+            print_word()
+    print("\n", end="")
+
