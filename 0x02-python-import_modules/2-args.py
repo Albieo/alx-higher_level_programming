8e9c1db0e9604108b1e5853df8957d83ccf73fc0
Albert Mpepo <albertmpepo@gmail.com>
2023-08-11 02:04:56 -0700

    functions added to pervent code from exec when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    argument_count = len(sys.argv) - 1
 
-    print(f"{argument_count} arguments:")
-    for i in range(1, len(sys.argv)):
-        print(f"{i}: {sys.argv[i]}")
+def main():
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        argument_count = len(sys.argv) - 1
+
+        print(f"{argument_count} arguments:")
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
+
+
+if __name__ in "__main__":
+    main()
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 1:
-    print(0)
-elif len(sys.argv) == 2:
-    pass
-else:
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print(sum)
+
+def main():
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        pass
+    else:
+        sum = 0
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print(sum)
+
+
+if __name__ == "__main__":
+    main()
