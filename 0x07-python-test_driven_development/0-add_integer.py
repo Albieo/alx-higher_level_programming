48ca7445876108e51e7953838426b4949686f507
Albert Mpepo <albertmpepo@gmail.com>
2023-09-01 10:18:33 +0200

    task 0 without doctests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Integers addition """
+
+
+def add_integer(a, b=98):
+    """Function that adds 2 integers
+
+    Note:
+        if args a or b are of type float the program
+        would convert the values to of type int.
+
+    Args:
+        a (int, float): first number to be added.
+        b (int, float): second number to be added.
+
+    Returns:
+        the sum of int a and b.
+
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
