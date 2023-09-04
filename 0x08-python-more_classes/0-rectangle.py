e0e5139b0c8a4a62209f0ae58a35d4e0de2bee9b
Albert Mpepo <albertmpepo@gmail.com>
2023-09-04 17:37:01 +0200

    0 Simple rectanagle fix 1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,4 +3,12 @@
 
 
 class Rectangle():
+    """
+    A class representing a rectangle shape.
+
+    This class provides a simple way to create a rectangle.
+
+    Attributes:
+        None
+    """
     pass
