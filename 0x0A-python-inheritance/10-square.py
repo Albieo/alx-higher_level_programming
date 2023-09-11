36ab6287b76f5bd60612eb22a76b445854ed6c2b
Albert Mpepo <albertmpepo@gmail.com>
2023-09-12 00:40:59 +0200

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Square #1 """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square, which is a special case of
+    a rectangle with equal sides.
+
+    Attributes:
+        __size (int): The size of each side of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square object with the given size.
+
+        Args:
+            size (int): The size of each side of the square.
+        """
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square, which is equal to the size squared.
+        """
+        return self.__size ** 2
