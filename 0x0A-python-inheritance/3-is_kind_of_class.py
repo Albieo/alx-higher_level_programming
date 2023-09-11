1212023f869c356bafae61101a06d925387c16b7
Albert Mpepo <albertmpepo@gmail.com>
2023-09-11 23:11:20 +0200

    3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Same class or inherit from """
+
+
 def is_kind_of_class(obj, a_class):
     """
     Check if an object is an instance of a given class or a subclass thereof.
