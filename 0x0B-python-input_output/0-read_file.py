82d1c11550b3b1fa4d08c068481138643518d377
Albert Mpepo <albertmpepo@gmail.com>
2023-09-12 21:47:12 +0200

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Reads and prints the contents of a text file line by line.
+
+    Args:
+        filename (str): The name of the file to be read. If not provided,
+            an empty string is used, which will result in an error when
+            attempting to open the file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist or
+                           cannot be found.
+        IsADirectoryError: If the provided filename refers to a directory
+                           instead of a regular file.
+
+    """
+    with open(filename) as file:
+        for lines in file:
+            print(lines)
