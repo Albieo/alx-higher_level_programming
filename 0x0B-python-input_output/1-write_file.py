73dc55ffef8512a6ae8002c4e73cf92dbb2b6e56
Albert Mpepo <albertmpepo@gmail.com>
2023-09-12 21:55:06 +0200

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  Write to a file """
+
+
+def write_file(filename="", text=""):
+    """
+    Write text to a file specified by the given filename.
+
+    Parameters:
+        filename (str): The name of the file to write to. If not provided,
+                        an empty string is used.
+        text (str): The text to be written to the file. If not provided,
+                    an empty string is used.
+
+    """
+    with open(filename, "w") as file:
+        file.write(text)
