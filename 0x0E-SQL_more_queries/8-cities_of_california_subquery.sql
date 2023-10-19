cb42731cefebdd93108e1c107170788495281814
Albert Mpepo <albertmpepo@gmail.com>
2023-10-19 02:10:59 +0200

    8. Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- Cities of California
+-- a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT name FROM cities WHERE states_id = (SELECT id FROM states WHERE name = "California");
