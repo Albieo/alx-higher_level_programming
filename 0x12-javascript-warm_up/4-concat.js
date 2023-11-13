5901db139617215754d1370ed9eb15f267636f46
Albert Mpepo <albertmpepo@gmail.com>
2023-11-13 18:49:38 +0200

    js document fix 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-/*
-    Write a script that prints two arguments passed to it, in the following format: “ is ”
-    
-    -> You must use console.log(...) to print all output
-    -> You are not allowed to use var
-*/
-
-
-const first = process.argv[2]
-const second = process.argv[3]
-
-console.log(first," is ", second)
+const first = process.argv[2];
+const second = process.argv[3];
+console.log(first, ' is ', second);
