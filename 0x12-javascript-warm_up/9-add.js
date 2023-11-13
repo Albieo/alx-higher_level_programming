9ebafbb27473d0f3216f5a9015eeb60db887eecf
Albert Mpepo <albertmpepo@gmail.com>
2023-11-13 21:01:24 +0200

    9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-/*
-    Write a script that prints the addition of 2 integers
-    
-    -> The first argument is the first integer
-    -> The second argument is the second integer
-    -> You have to define a function with this prototype: function add(a, b)
-    -> You must use console.log(...) to print all output
-    -> You are not allowed to use var
-*/
+function add(a, b) {
+    if (a == undefined || b == undefined) {
+        return (NaN);
+    } else {
+        return (parseInt(a) + parseInt(b));
+    }
+}
+console.log(add(process.argv[2], process.argv[3]));
