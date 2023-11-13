f2aff9526b36cbaf235ff064672e53a590468389
Albert Mpepo <albertmpepo@gmail.com>
2023-11-13 22:20:45 +0200

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
+if (process.argv.length === 2) {
     console.log('Not a number');
 } else {
-    console.log('OK');
-};
+    number = process.argv[2];
+    if (Number.isInteger(parseInt(number))) {
+        console.log('My number: %s', Math.floor(number));
+    } else {
+        console.log('Not a number');
+    }
+}
