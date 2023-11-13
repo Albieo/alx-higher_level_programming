97d360267bd4528ac559122073039cdf17d50156
Albert Mpepo <albertmpepo@gmail.com>
2023-11-13 19:07:33 +0200

    js document fix 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 if (process.argv.length <= 2) {
     console.log('Missing number of occurrences');
-}
-
-number = parseInt(process.argv[2]);
-
-while (number > 0) {
-    console.log('C is fun');
-    number -= 1;
+} else {
+    number = parseInt(process.argv[2]);
+    while (number > 0) {
+        console.log('C is fun');
+        number -= 1;
+    }
 }
