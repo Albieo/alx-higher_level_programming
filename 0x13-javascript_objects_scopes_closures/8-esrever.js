e45abcb88a92aceb6099d5466f9ab9e58df4dbcf
Albert Mpepo <albertmpepo@gmail.com>
2023-11-14 20:14:37 +0200

    8. Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   reversed_list = [];
-  for (element of list) {
-    reversed_list += element;
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed_list.push(list[i]);
   }
   return (reversed_list);
 }
