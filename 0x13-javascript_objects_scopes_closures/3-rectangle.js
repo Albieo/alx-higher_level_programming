5def739fe2d954a4a6e4a88ff3becaca887e226c
Albert Mpepo <albertmpepo@gmail.com>
2023-11-15 00:46:17 +0200

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-    let line = '';
+      let line = '';
       for (let j = 0; j < this.width; j++) {
         line += 'X';
       }
