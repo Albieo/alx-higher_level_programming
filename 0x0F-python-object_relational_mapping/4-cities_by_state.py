81ad54bc825267512598f370c21907e0941bfb52
Albert Mpepo <Albert Mpepo@LAPTOP-MO59H0V7>
2023-11-19 03:07:35 +0200

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM cities")
+    cur.execute("SELECT * FROM cities ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
