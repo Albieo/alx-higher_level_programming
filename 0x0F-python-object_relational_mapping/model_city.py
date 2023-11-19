53bbc23431c625f40720183b70ecdc2e9dcfb54d
Albert Mpepo <Albert Mpepo@LAPTOP-MO59H0V7>
2023-11-19 08:33:16 +0200

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a python file that contains the class definition
+of a City that inherits the State model """
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """ a class representation of the City database model """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
