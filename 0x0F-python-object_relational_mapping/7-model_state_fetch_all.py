afbbd821fba27699f6a91616862b53d2bb6cced4
Albert Mpepo <Albert Mpepo@LAPTOP-MO59H0V7>
2023-11-19 06:45:43 +0200

    7. All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,11 +10,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
     states = session.query(State).order_by(State.id)
-
     for state in states:
         print("{}: {}".format(states.id, states.name))
