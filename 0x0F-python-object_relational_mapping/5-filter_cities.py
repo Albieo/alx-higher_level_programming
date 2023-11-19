5f1a97e842297b249a69840b5ee17d6f87e8fabe
Albert Mpepo <Albert Mpepo@LAPTOP-MO59H0V7>
2023-11-19 03:30:10 +0200

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  a script that lists all cities from the database hbtn_0e_4_usa """
+"""  a script that takes in the name of a state as an argument and
+     lists all cities of that state, using the database hbtn_0e_4_usa """
 import MySQLdb
 import sys
 
@@ -14,7 +15,7 @@ if __name__ == "__main__":
                     FROM cities INNER JOIN states ON states.id=cities.state_id
                     WHERE states.name = %s""", (state_name,))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    temp = list(row[0] for row in rows)
+    print(*temp, sep=", ")
     cur.close()
     db.close()
