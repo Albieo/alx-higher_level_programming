2a407dfb25c52701322d6872fd069be1cc47786e
Albert Mpepo <albertmpepo@gmail.com>
2023-12-01 17:56:00 +0200

    read me added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 import urllib.request
 
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    raw_body = response.read()
-    body = raw_body.decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        raw_body = response.read()
+        body = raw_body.decode('utf-8')
 
-print("Body response:")
-print("\t- type:", type(raw_body))
-print("\t- content:", raw_body)
-print("\t- utf8 content:", body)
+    print("Body response:")
+    print("\t- type:", type(raw_body))
+    print("\t- content:", raw_body)
+    print("\t- utf8 content:", body)
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+Python Newworking 1 uses Python to communicate with APIs and Servers
