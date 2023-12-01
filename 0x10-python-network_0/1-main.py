75749cb1e24b3d99510ada2e1248601ab4e28560
Albert Mpepo <albertmpepo@gmail.com>
2023-12-01 10:32:08 +0200

    1. cURL to the end

--- 0x10-python-network_0/1-main.py
+++ 0x10-python-network_0/1-main.py
@@ -4,19 +4,14 @@ import sys
 
 
 def get_response_body(url):
-    command = f"curl -s -o response.txt -w '%{{http_code}}' {url}"
+    command = f"curl -s -o /dev/null -w '%{http_code}' -X GET {url}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
     if result.returncode == 0:
-        http_code = result.stdout.strip()
-        if http_code == '200':
-            with open('response.txt', 'r') as file:
-                body = file.read()
-                print(f"Body of the response (Status Code 200):\n{body}")
-        else:
-            print(f"Non-200 status code received: {http_code}")
+        print(result.stdout.strip())
     else:
-        print("Error occurred while fetching the response body.")
+        print("Error occurred while fetching response size.")
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
