b25de988e396463df57bf399a573ae58aa8de911
Albert Mpepo <albertmpepo@gmail.com>
2023-12-02 02:33:14 +0200

    3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(f"Response Body: {body}")
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
